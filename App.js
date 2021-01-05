09a41f1ec814259319c0be9d0b14e74b7050046a
Shubha1096 <74348224+Shubha1096@users.noreply.github.com>
2021-01-05 17:12:29 +0900

    codeChange

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 var inputNum = document.querySelector("#items");
 var inputDate = document.querySelector("#inputField");
 var outputDiv = document.querySelector("#result");
-var sum1 = 0;
+var errorMessage = document.querySelector("#errorField");
 
 function changeHandler() {
+    var sum1
     var birthDate = inputDate.value;
     var array = birthDate.split("-");
 
@@ -14,19 +15,23 @@ function changeHandler() {
 
         }
     }
-
+   
     var luckyNum = inputNum.value;
-    if (birthDate === "") {
-        var commentx = "Please input correct Birthdate"
-        outputDiv.innerText = commentx;
-        inputDate.addEventListener("change", changeHandler);
-    } else if (sum1 % luckyNum === 0) {
-        var comment1 = "Your lucky number is divisible by sum of all the digits of your birthdate"
-        outputDiv.innerText = comment1;
-    } else {
-        var comment2 = "Your lucky number is not divisible by sum of all the digits of your birthdate"
-        outputDiv.innerText = comment2;
+    if (sum1 !== undefined && luckyNum !== null) {
+        if (sum1 % luckyNum === 0) {
+            var comment1 = "Your lucky number is divisible by sum of all the digits of your birthdate"
+            outputDiv.innerText = comment1;
+        } else {
+            var comment2 = "Your lucky number is not divisible by sum of all the digits of your birthdate"
+            outputDiv.innerText = comment2;
+        }
+    } else if (sum1 === undefined){
+        var comment3 = "*Please input correct DOB"
+        errorMessage.innerText = comment3;
+    } else{
+        var comment4 = "Please input correct number"
+        errorMessage.innerText = comment4;
     }
 }
 
-inputNum.addEventListener("change", changeHandler);
+inputNum.addEventListener("change", changeHandler); 
